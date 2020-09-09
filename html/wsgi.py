cd5bf7c99dc8a0262119ddcab685f4311be1720f
Roy Vorster <royvorster@gmail.com>
2020-09-09 12:07:21 +0200

    Add simple jinja template, quick rewrite, test form

--- html/wsgi.py
+++ html/wsgi.py
@@ -1,7 +1,33 @@
-# Main wsgi application
+'''
+Main page with form and button
+    POST: Store text in some folder -> later: markdown
 
-def application(environ,start_response):
-    status, response_header = '200 OK', [('Content-type','text/html')]
-    start_response(status,response_header)
+    Get all files in that folder and output. Jinja templating engine
+'''
 
-    return [b"LOL"]
+from jinja2 import Template
+
+class Application:
+    def __init__(self, environ, start_response):
+        self.environ, self.start_response = environ, start_response
+
+    def __iter__(self):
+        self.start_response(get_status(), get_response_header())
+
+
+        html = self.render()
+        yield str.encode(html)
+
+    def render(self):
+        with open('index.html.j2') as f:
+            page = Template(f.read())
+
+        page.render(some_text='lol')
+
+        return page
+
+    def get_status():
+        return '200 OK'
+
+    def get_response_header():
+        return [('Content-type','text/html')]
